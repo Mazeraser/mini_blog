dbbcba0a79eb781eb02a822c468f25bcd430bdd2
BossOfThisGym <danila.solowyow2016@yandex.ru>
2021-05-05 20:20:55 +0300

    add bloggers page

--- bloggers/urls.py
+++ bloggers/urls.py
@@ -3,5 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-	path('accounts/',include('django.contrib.auth.urls'))
+	path('register/', views.register, name='register'),
+	path('accounts/',include('django.contrib.auth.urls')),
+	path('accounts/<str:username>',views.user,name='blogger_page')
 ]
--- bloggers/views.py
+++ bloggers/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from blogs.models import Post
+from .models import BloggerInfo
 
-# Create your views here.
+def user(request,username):
+	info_user = {
+		'user':BloggerInfo.objects.create(name=username),
+		'posts':Post.objects.filter(Author=User.objects.filter(username=username)[:1],Date__lte=timezone.now()).order_by('Date')
+	}
+	return render(request,'blogger_page.html',info_user)
