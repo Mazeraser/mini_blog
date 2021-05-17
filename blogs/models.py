4a6ed0fffca7bee12d6eb27920403c89ecf33fae
BossOfThisGym <danila.solowyow2016@yandex.ru>
2021-05-17 18:59:43 +0300

    Пофиксил редактирование
    Теперь изменить пост может только его создатель

--- blogs/models.py
+++ blogs/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     Date = models.DateTimeField(default=timezone.now)
 
     class Meta:
+        permissions = ()
         ordering = ["-Title"]
 
     def publish(self):
--- blogs/views.py
+++ blogs/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, get_object_or_404,redirect
+from django.shortcuts import render, get_object_or_404,redirect,Http404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def post_new(request):
 	form = PostForm()
+	redac = 0
 	if request.method == "POST":
 		form = PostForm(request.POST)
 		if form.is_valid():
@@ -26,12 +28,15 @@ def blog_change(request,pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method == "POST":
 		form = PostForm(request.POST, instance=post)
-		if form.is_valid():
-			post = form.save(commit=False)
-			post.Author = request.user
-			post.Date = timezone.now()
-			post.save()
-			return redirect('post_info', pk=post.pk)
+		if post.Author != request.user:
+			return redirect('Home')
+		else:
+			if form.is_valid():
+				post_ch = form.save(commit=False)
+				post_ch.Author = request.user
+				post_ch.Date = timezone.now()
+				post_ch.save()
+				return redirect('post_info', pk=post_ch.pk)
 	else:
 		form = PostForm(instance=post)
 	return render(request, 'blog/post_new.html', {'form': form})
